7e270ad06215adb0853386f5566182db292245b0
Chengxi (Chen) Shi <chengxis@usc.edu>
2015-11-01 19:47:08 -0800

    Update main.cpp
    workable one, with help from online

--- main.cpp
+++ main.cpp
@@ -1,59 +1,36 @@
 class Solution {
 public:
+    
+    bool isPalindrome(string str){
+        int start = 0, end = str.length() - 1;
+        while(start <= end){
+            if(str[start] != str[end]) return false;
+            start ++;
+            end --;
+        }
+        return true;
+    }
+    
+    void myPartition(string &s, int start, vector<string> &temp, vector<vector<string>> &solution){
+        if(start == s.length() ){
+            solution.push_back(temp);
+            return;
+        }
 
-    string addbars(string str){
-        string tmp = "";
-        for(int i = 0; i < str.length(); i ++){
-            tmp += "#";
-            tmp += str[i];
+        for(int i = start; i < s.length(); i ++){
+            if(isPalindrome(s.substr(start, i - start + 1))){
+                temp.push_back(s.substr(start, i - start + 1));
+                myPartition(s, i + 1, temp, solution);
+                temp.pop_back();
+            }            
         }
-        tmp += "#";
-        return tmp;
-    }
-    
-    string removePonds(string str){
-        string tmp = "";
-        int ptr = 0;
-        while(ptr < str.length()){
-            if(str[ptr] != '#'){
-                tmp += str[ptr];
-            }
-            ptr++;
-        }
-        return tmp;
-    }
-    
-    vector<vector<string>> myPartition(string s){
-        vector<vector<string>> arr;
-        if(s.length() == 0) return arr;
-        int ptr = s.length() - 1;
-        while(ptr >= 0){
-            int left = ptr;
-            int right = ptr;
-            if(s[ptr] == '#'){
-                left -= 1;
-                right += 1;
-            }
-            while(left >= 0 && right < s.length()){
-                if(s[left] != '#' && s[left] == s[right]){
-                    vector<vector<string>> tmparr = myPartition(s.substr(right + 1));
-                    for(vector<string> elem : tmparr){
-                        elem.insert(elem.begin(), removePonds(s.substr(left, right-left)));
-                    }
-                }
-                left -= 2;
-                right += 2;
-            }//end while
-            ptr--;
-        }//end while
-        return arr;
     }
     
     vector<vector<string>> partition(string s) {
         vector<vector<string>> arr;
         if(s.length() == 0) return arr;
-        string str = addbars(s);
-        arr = myPartition(str);
+        vector<string> output;
+        myPartition(s, 0,output, arr);
         return arr;
     }
 };
